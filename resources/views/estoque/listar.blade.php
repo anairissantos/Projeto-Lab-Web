db4c19eaf15b6e11ca9eb3b267a311a02ac7ad30
Ícaro Erasmo <erasmo.iesb@gmail.com>
2017-08-25 20:26:51 -0300

    alteração na view

--- resources/views/estoque/listar.blade.php
+++ resources/views/estoque/listar.blade.php
@@ -10,7 +10,7 @@
 
                         <div class="form-group">
                             @foreach($armamentos as $armamento)
-                                <label class="col-md-4 control-label form-inline">{{$armamento->item->descricao}}: {{$armamento->quantidade}}</label>
+                                <label class="col-md-4 control-label form-inline">{{$armamento->item->modelo}}: {{$armamento->disponivel}}</label>
                             @endforeach
                         </div>
                     </div>
