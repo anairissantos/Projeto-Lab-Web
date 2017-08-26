eab4ee04aaa492b6f59fb54b5ae2dd7b93349aad
Ícaro Erasmo <erasmo.iesb@gmail.com>
2017-08-26 16:49:51 -0300

    teste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
 </head>
 <body>
   @if (!Auth::guest())
-<div class="col-md-2 pull-left">
+<div class="row">
     <nav class="navbar navbar-default sidebar" role="navigation">
         <div class="container-fluid">
         <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
