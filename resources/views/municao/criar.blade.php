d7195b54f088859eb37f87893d1e9fd5d8fadf2d
Ícaro Erasmo <erasmo.iesb@gmail.com>
2017-08-26 15:29:16 -0300

    teste

--- resources/views/municao/criar.blade.php
+++ resources/views/municao/criar.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <!--<div class="container">-->
         <div class="row">
             <div class="col-md-8">
                 <div class="panel panel-default">
@@ -64,5 +64,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    <!--</div>-->
 @endsection
