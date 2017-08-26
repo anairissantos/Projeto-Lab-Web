a2305d8550061ddba5af09ef2bec6ef3dcedc8eb
Ícaro Erasmo <erasmo.iesb@gmail.com>
2017-08-25 21:04:57 -0300

    resolução de treta

--- resources/views/estoque/listar.blade.php
+++ resources/views/estoque/listar.blade.php
@@ -10,7 +10,7 @@
 
                         <div class="form-group">
                             @foreach($armamentos as $armamento)
-                                <label class="col-md-4 control-label form-inline">{{$armamento->modelo}}: @if($armamento->disponivel) Sim @else Não</label> @endif
+                                <label class="col-md-4 control-label form-inline">{{$armamento->modelo}}:@if($armamento->disponivel)Sim@elseNão</label>@endif
                             @endforeach
                         </div>
                     </div>
